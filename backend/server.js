ca869a58a17b7076340daa9ba9242f00e4c4ce86
maldisco <filipe.sf04@gmail.com>
2023-10-07 10:50:12 -0300

    vercel didnt work

--- backend/server.js
+++ backend/server.js
@@ -51,72 +51,8 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+app.use("/api/animes", animeRoutes);
 
-function normalizeAndClean(str) {
-  if (!str) return null;
-  const normalized = str
-    .replace(/[^a-zA-Z ]/g, "")
-    .trim()
-    .toLowerCase();
-  // Replace "x" with " ", and remove multiple spaces
-  return normalized.replace(/x/g, " ").replace(/ +/g, "");
-}
-
-app.get("/api/animes/nomes", (req, res, next) => {
-  Anime.find().then(
-    (data) => {
-      var nomes = [];
-      var uniqueNames = new Set(); // Use a Set to ensure uniqueness
-
-      data.forEach((element) => {
-        const cleanNome1 = normalizeAndClean(element.nome);
-        const cleanNome2 = element.nome2
-          ? normalizeAndClean(element.nome2)
-          : null;
-
-        // Add cleanNome1 if it's unique
-        if (cleanNome1 && !uniqueNames.has(cleanNome1)) {
-          uniqueNames.add(cleanNome1);
-          nomes.push(element.nome);
-        }
-
-        // Add cleanNome2 if it's unique
-        if (cleanNome2 && !uniqueNames.has(cleanNome2)) {
-          uniqueNames.add(cleanNome2);
-          nomes.push(element.nome2);
-        }
-      });
-
-      res.status(200).json(nomes);
-    },
-    (err) => {
-      res.json({ message: err.message });
-    }
-  );
-});
-
-app.post("/api/animes/aleatorio", (req, res, next) => {
-  const excludedIds = req.body.excludedIds || [];
-  const excludedObjIds = excludedIds.map((id) => {
-    return new mongoose.Types.ObjectId(id);
-  });
-  /* return a random anime */
-  Anime.aggregate([
-    {
-      $match: {
-        _id: { $nin: excludedObjIds },
-      },
-    },
-    { $sample: { size: 1 } },
-  ]).then(
-    (data) => {
-      res.status(200).json(data[0]);
-    },
-    (err) => {
-      res.json({ message: err.message });
-    }
-  );
-});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening on port ${process.env.PORT}`);

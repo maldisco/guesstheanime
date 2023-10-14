6f469715c45cadff91e98d6f364f85bf8e0a9979
maldisco <filipe.sf04@gmail.com>
2023-10-14 10:39:04 -0300

    remove wrong dependency

--- frontend/src/components/AnimeImage.jsx
+++ frontend/src/components/AnimeImage.jsx
@@ -1,6 +1,10 @@
-import { Box, useTheme } from "@mui/material";
-import { render } from "@testing-library/react";
-
+/**
+ * A component that displays an anime image with a given height.
+ * @param {Object} props - The component props.
+ * @param {string} props.image - The URL of the anime image.
+ * @param {number} props.height - The height of the anime image in viewport height units.
+ * @returns {JSX.Element} - The AnimeImage component.
+ */
 const AnimeImage = ({ image, height }) => {
   const imageSizeStyle = {
     objectFit: "cover",

60d7643d9db7183fc1e868673ab44839511bd7e1
maldisco <filipe.sf04@gmail.com>
2024-12-31 12:03:19 -0300

    fix: unique names

--- frontend/src/widgets/AnimeQuiz.jsx
+++ frontend/src/widgets/AnimeQuiz.jsx
@@ -20,20 +20,7 @@ import GuessInput from "components/GuessInput";
 import { useDispatch } from "react-redux";
 import { useState, useEffect } from "react";
 
-/**
- * A component that renders a quiz game for guessing anime names.
- * @param {Object} props - The component props.
- * @param {number} props.guessNumber - The current guess number.
- * @param {Array} props.previousGuesses - The list of previous guesses.
- * @param {Object} props.current - The current anime being guessed.
- * @param {Array} props.alreadyGuessed - The list of already guessed animes.
- * @param {boolean} props.finished - A flag indicating if the game has finished.
- * @param {boolean} props.correct - A flag indicating if the current guess is correct.
- * @param {Array} props.animeList - The list of all available animes.
- * @param {Object} props.tipsNumber - The number of tips available for each category.
- * @param {Object} props.tipsColor - The color of each tip category.
- * @returns {JSX.Element} - The AnimeQuiz component.
- */
+
 const AnimeQuiz = ({
   guessNumber,
   previousGuesses,
@@ -56,11 +43,11 @@ const AnimeQuiz = ({
   const normalizeAndClean = (str) => {
     if (!str) return null;
     const normalized = str
-      .replace(/[^a-zA-Z ]/g, "")
+      .replace(/[^a-zA-Z0-9 ]/g, "")
       .trim()
       .toLowerCase();
     // Replace "x" with " ", and remove multiple spaces
-    return normalized.replace(/x/g, " ").replace(/ +/g, "");
+    return normalized.replace(/ +/g, "");
   };
 
   const getUniqueNames = (animes) => {
@@ -77,10 +64,7 @@ const AnimeQuiz = ({
       if (cleanNome1 && !uniqueNames.has(cleanNome1)) {
         uniqueNames.add(cleanNome1);
         nomes.push(element.nome);
-      }
-
-      // Add cleanNome2 if it's unique
-      if (cleanNome2 && !uniqueNames.has(cleanNome2)) {
+      } else if (cleanNome2 && !uniqueNames.has(cleanNome2)) {
         uniqueNames.add(cleanNome2);
         nomes.push(element.nome2);
       }
@@ -139,17 +123,13 @@ const AnimeQuiz = ({
       (anime) => !alreadyGuessedAnimeNames.includes(anime.nome)
     );
 
-    // if there is no anime in cache
     if (!current) {
-      // Get a new anime
       const randomAnime = getRandomAnime(possibleAnime);
       dispatch(setCurrent(randomAnime));
       randomizeTips();
     }
 
-    // Get all anime names for the autocomplete
     setAnimeNames(getUniqueNames(possibleAnime));
-
     setIsLoading(false);
   };
 
@@ -161,20 +141,13 @@ const AnimeQuiz = ({
     if (!guess) return;
     setIsProcessingGuess(true);
     if (guess === current.nome || guess === current.nome2) {
-      // Save the correct guess number to cache
       dispatch(addCorrectGuess(guessNumber));
-      // set correct flag
       dispatch(setCorrect(true));
-      // set finish flag
       dispatch(setFinished(true));
-      // Save current list of previous guesses to cache
       dispatch(setPreviousGuesses([...previousGuesses, guess]));
     } else {
-      // Add 1 to the current guess number and save it in cache
       dispatch(setGuessNumber(guessNumber + 1));
-      // Add the current guess to the list of previous guesses and save it in cache
       dispatch(setPreviousGuesses([...previousGuesses, guess]));
-      // set finish flag
       dispatch(setFinished(guessNumber === 5));
     }
     setIsProcessingGuess(false);
@@ -182,18 +155,15 @@ const AnimeQuiz = ({
 
   const reset = async () => {
     setIsLoading(true);
-    // while loading
     {
-      // Add the current anime and if user got it to the list of already guessed animes
       const guessedAnime = {
         anime: current.nome,
         isCorrect: correct,
       };
-      // remove the first animes of the list until there is only 10
+
       const updatedAlreadyGuessed = [...alreadyGuessed, guessedAnime];
       dispatch(setAlreadyGuessed(updatedAlreadyGuessed));
 
-      // Get a new anime
       const alreadyGuessedAnimeNames = alreadyGuessed.map(
         (anime) => anime.anime
       );
@@ -204,7 +174,6 @@ const AnimeQuiz = ({
       const randomAnime = getRandomAnime(possibleAnime);
       dispatch(setCurrent(randomAnime));
 
-      // randomize tips order
       randomizeTips();
 
       setGuess("");
@@ -215,9 +184,7 @@ const AnimeQuiz = ({
 
   const skipGuess = async () => {
     if (guessNumber <= 5) {
-      // Add 1 to the current guess number and save it in cache
       dispatch(setGuessNumber(guessNumber + 1));
-      // set finish flag
       dispatch(setFinished(guessNumber === 5));
     }
   };
